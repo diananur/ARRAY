b7e648e8cfdcbb5dc4633afc2b140c7b1f9a91b6
Lenovo <Lenovo@DESKTOP-SQV50RK>
2017-09-25 12:34:34 +0700

    modul 5 GUI

--- src/array_multi_dimensi/testarraydua.java
+++ src/array_multi_dimensi/testarraydua.java
@@ -20,7 +20,9 @@ System.out.println("Identitas Siswa Angkatan 24");
 public void namanis (int n){
 for (int i = 0; i< 1; i++){
 for (int j = 0; j < 1; j++){
+System.out.println(nama[0][j]+":"+nis[0][j]);
 System.out.println(nama[1][j]+":"+nis[1][j]);
+System.out.println(nama[2][j]+":"+nis[2][j]);
 }
 }
 }
@@ -28,6 +30,7 @@ System.out.println(nama[1][j]+":"+nis[1][j]);
 public static void main (String[] args){
 testarraydua siswa = new testarraydua();
 siswa.tampil();
+siswa.namanis(0);
 }
 
 }
